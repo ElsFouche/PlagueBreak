e56544019c60fa5d307ad4daa190d8581220a588
ElsFouche <166535453+ElsFouche@users.noreply.github.com>
2025-09-23 18:24:27 -0700

    Wave System & Enemy Damage
    - Enemies are now damaged by completing matches
    - Waves auto-advance when the wave health is depleted
    - Enemies are destroyed visually based on the percentage of wave health remaining
    - Enemies are destroyed at random

--- Assets/Editor/GUI_DEBUG.cs
+++ Assets/Editor/GUI_DEBUG.cs
@@ -8,11 +8,18 @@ public class GUI_DEBUG : Editor
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
+        if (GUILayout.Button("Test Wave"))
+        {
+            var myScript = target as EnemyHandler;
+            myScript.UpdateWaveCount("Test");
+        }
+/*
         if (GUILayout.Button("Test Damage"))
         {
             var myScript = target as EnemyHandler;
             myScript.DealDamage(1.0f);
         }
+*/
     }
 }
 #endif
